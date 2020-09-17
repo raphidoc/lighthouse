8ab396b85518afc77d11f1c037d190e6dbdc040d
raphael <raphael.mabit@gmail.com>
2020-09-17 22:24:16 +0000

    rename and create documentation

--- R/l2_dir_list.R
+++ R/l2_dir_list.R
@@ -1,11 +1,18 @@
+#' Fuction to create directories.for.*.dat requierd by Cops and Riops packages
+#'
+#' @param path is the L2 folder path (string) where uniquely identifie stations can be found.
+#' @param what is a character vector of what to search for, default c("COPS","IOP")
+#' @param boat is a character vector of to filter by boat, default c("")
+#' @param upInDir logical indicating if update of file path append also in L2 subdirectories.
+#'
+#' @return nothing
+#'
 #' @export
 #'
 #'
 #'
 
 l2_dir_list <- function(path=getwd(), what=c("COPS","IOP"), boat=c(""), upInDir=T){
-	# fuction to create directories.for.*.dat
-	# path is the path
 
 	for (i in seq_along(what)) {
 		if (file.exists(paste0("directories.for.",tolower(what[i]),".dat"))) {

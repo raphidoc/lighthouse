c131b8b97a2bf75c9ea04cf80bcf2128656c7244
raphael <raphael.mabit@gmail.com>
2020-09-20 22:34:22 +0000

    Update check function, evaluation inside if statment seems od when calling the function from other package (dir.exists: no object L2)

---
+++ R/check_l3.R
@@ -0,0 +1,22 @@
+#' @author Raphael Mabit
+#' @description Check if databases are present for a specific set in L3.
+#' If so the function will create an archive to ensure no data is accidentally erased
+#' @export
+
+check_l3 <- function(project,L3,set) {
+
+  # Check if L3 exist (for set) and is not empty, if not create an archive
+  if (exists("L3") && exists("set") && dir.exists(file.path(L3,set)) &&
+      length(list.files(file.path(L3,set))) != 0) {
+
+    message("L3/",set," dir exist and is not empty, creating an archive.")
+    db <- list.files(file.path(L3,set), pattern = set,)
+    # change directory to create proper archive
+    setwd(file.path(L3,set))
+    tar(tarfile = paste0("archive_",Sys.Date(),".tar"))
+
+  } else if (!dir.exists(file.path(L3,set))) {
+    dir.create(file.path(L3,set), recursive = T)
+  }
+  setwd(project)
+}
--- R/check_project.R
+++ R/check_project.R
@@ -8,28 +8,30 @@
 #' @inheritParams L1
 #' @inheritParams L2
 #' @param set The set for which to check.
+#' @export
 
-check_project <- function(project, L1, L2, set = ""){
+check_project <- function(project, L1, L2, L3, set = "") {
 
 	Pobjects <- list.files(project, recursive = F, full.names = T)
 
 	# Check if project path is trully a project folder root
-	if (exists("L1") & dir.exists(L1)) {
+	if (exists("L1") && dir.exists(L1)) {
 
 		Proot <- T
 	} else { Proot <- F }
 
-	if (file.exists(grep("data_synthesis|Data_Synthesis", Pobjects, value = T))) {
+	if (file.exists(grep("data_synthesis", Pobjects, value = T))) {
 
 		Proot <- T
 	} else { Proot <- F }
 
-	# Check if L2 exist (for param) and is not empty
-	if (exists(c("L2","set")) & dir.exists(L2) &
+	# Check if L2 exist (for set) and is not empty
+	if (exists("L2") && exists("set") && dir.exists(L2) &&
 	    length(dir(L2, pattern = set, recursive = T ,include.dirs = T)) != 0) {
 
 		L2exist <- T
-	}  else { L2exist <- F }
+	} else { L2exist <- F
+		}
 
 	list("Proot" = Proot, "L2exists"=L2exist)
 

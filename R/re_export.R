9ec0527b1b4502e9ec4b2bff47e99603a33e0eca
raphael <raphael.mabit@gmail.com>
2020-09-25 14:04:35 +0000

    proper re_export for Simon R packages

---
+++ R/re_export.R
@@ -0,0 +1,24 @@
+
+# Cops --------------------------------------------------------------------
+
+#' @importFrom Cops cops_go
+#' @export
+Cops::cops_go
+
+#' @importFrom Cops generate_cops_db
+#' @export
+Cops::generate_cops_db
+
+#' @importFrom Cops cops_detection_limit
+#' @export
+Cops::cops_detection_limit
+
+# Riops -------------------------------------------------------------------
+
+#' @importFrom Riops iop_go
+#' @export
+Riops::iop_go
+
+#' @importFrom Riops generate_iop_db
+#' @export
+Riops::generate_iop_db

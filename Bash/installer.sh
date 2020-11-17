1e934c98f6e15fe3cecbe2bec376f32f4c764c96
raphael <raphael.mabit@gmail.com>
2020-11-17 20:19:58 +0000

    First dashboard

--- Bash/installer.sh
+++ Bash/installer.sh
@@ -2,32 +2,189 @@
 # Script to install lighthouse, Cops, Riop, RspectroAbs, ASD, HyperocR
 # and their dependencies.
 
-# lighthouse
-/usr/bin/git clone https://github.com/raphidoc/lighthouse.git ~/R/lighthouse
-/usr/bin/git fetch
-/usr/bin/git pull
+# Set CRAN mirror
+mirror="https://muug.ca/mirror/cran/"
 
-# Cops
-/usr/bin/git clone https://github.com/raphidoc/Cops.git ~/R/Cops
-/usr/bin/git fetch
-/usr/bin/git pull
+# uncomment the above line to install devtools
+#R -e "install.packages(\"devtools\", repos = \"$mirror\")"
 
-# Riops
-/usr/bin/git clone https://github.com/raphidoc/Riops.git ~/R/Riops
-/usr/bin/git fetch
-/usr/bin/git pull
+echo -e  "\nCops"
+GitRemote="git@srscm03.uqar.ca:mabr0002/Cops.git"
+DIR=~/R/Cops
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
 
-# RspectroAbs
-/usr/bin/git clone https://github.com/raphidoc/RspectroAbs.git ~/R/RspectroAbs
-/usr/bin/git fetch
-/usr/bin/git pull
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
 
-# HyperocR
-/usr/bin/git clone https://github.com/raphidoc/HyperocR.git ~/R/HyperocR
-/usr/bin/git fetch
-/usr/bin/git pull
 
-# ASD
-/usr/bin/git clone https://github.com/raphidoc/ASD.git ~/R/ASD
-/usr/bin/git fetch
-/usr/bin/git pull
+echo -e  "\nRiops"
+GitRemote="git@srscm03.uqar.ca:mabr0002/Riops.git"
+DIR=~/R/Riops
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
+
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
+
+echo -e  "\nRspectroAbs"
+GitRemote="git@srscm03.uqar.ca:mabr0002/RspectroAbs.git"
+DIR=~/R/RspectroAbs
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
+
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
+
+echo -e "\nHyperocR"
+GitRemote="git@srscm03.uqar.ca:mabr0002/HyperocR.git"
+DIR=~/R/HyperocR
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
+
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
+
+echo -e "\nASD"
+GitRemote="git@srscm03.uqar.ca:mabr0002/asd.git"
+DIR=~/R/ASD
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
+
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
+
+echo -e "\nlighthouse"
+GitRemote="git@srscm03.uqar.ca:mabr0002/lighthouse.git"
+DIR=~/R/lighthouse
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Directory ${DIR} exist, checking for update..."
+  cd $DIR
+  git fetch
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL != $REMOTE ]; then
+    echo "Pulling from ${GitRemote}"
+    /usr/bin/git pull
+    cd ~
+    echo
+    R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+    echo
+  else
+    echo "Alredy up to date"
+  fi
+
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning ${GitRemote} in ${DIR}"
+  /usr/bin/git clone $GitRemote $DIR
+  echo
+  R -e "devtools::install(\"$DIR\", repos = \"$mirror\")"
+  echo
+fi
